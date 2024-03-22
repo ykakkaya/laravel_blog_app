e178a9edc0dd65d0abd042180bd4035c0195ad1c
Yakup Akkaya <100940437+ykakkaya@users.noreply.github.com>
2024-03-22 09:34:29 +0300

    yk

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ use App\Http\Controllers\Front\HomeController;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth','verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
